9d1dda40ff4e559122c8c85eaed4aa5263347582
junaid-hossain <158271661+junaid-h0ssain@users.noreply.github.com>
2026-05-24 09:58:31 +0600

    init

---
+++ src/main/java/com/dokan/shppr/ShpprApplication.java
@@ -0,0 +1,13 @@
+package com.dokan.shppr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShpprApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShpprApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dokan/shppr/ShpprApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dokan.shppr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShpprApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
